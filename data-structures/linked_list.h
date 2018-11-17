22b9e3652d4d5b07880b683a94fe5af349ee1d7e
Luddha <tuxbuddha@gmail.com>
2018-11-17 13:57:59 -0600

    finished playing with linked_list class

--- data-structures/linked_list.h
+++ data-structures/linked_list.h
@@ -1,63 +1,24 @@
 #pragma once
 
-typedef struct node {
-    struct node *next;
-    struct node *prev;
-    int data;
-} node;
-
 class linked_list{
     private:
+    	typedef struct node {
+		struct node *next;
+		struct node *prev;
+		int data;
+	} node;
         node *n;
         node *head;
         node *tail;
         int list_size;
     public:
-        linked_list(){
-            this->n = new node;
-            this->n->data = 0;
-            this->n->next = nullptr;
-            this->n->prev = nullptr;
-            this->head = this->n;
-            this->tail = this->n;
-            this->list_size = 1;
-        }
-        linked_list(int d){
-            this->n = new node;
-            this->n->data = d;
-            this->n->next = nullptr;
-            this->n->prev = nullptr;
-            this->head = n;
-            this->tail = n;
-            this->list_size = 1;
-        }
-        void append(int d){
-            node *train = this->head;
-            node *new_node = new node;
-            new_node->data = d;
-            new_node->next = nullptr;
-            new_node->prev = nullptr;
-            while (train->next != nullptr)
-                train = train->next;
-            
-            train->next = new_node;
-            new_node->prev = train;
-            this->tail = new_node;
-            this->list_size++;
-        }
-
-        int pop(){
-            int data = this->tail->data;
-            node *tmp = this->tail;
-            this->tail = this->tail->prev;
-            delete tmp;
-            return data;
-        }
-
-        ~linked_list(){
-            node *train = this->tail;
-            while (train->prev != nullptr)
-                this->pop();
-            this->pop();
-        }
-}
+        linked_list();
+        linked_list(int d);
+        void append(int d);
+        int pop();
+	const int get_data(int index);
+	int set_data(int index, int d);
+	
+	const void show_list();
+        ~linked_list();
+};
